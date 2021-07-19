b04333cbf110baecfdb94a9705824f6275d95a25
Darshan A V <avd30675@gmail.com>
2021-07-19 16:12:06 +0530

    connected to database

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
 from flask import Flask,render_template,url_for
 from forms import *
+from modules import *
+
 
 app=Flask(__name__)
 
 app.secret_key='replace later'
 
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://xfwrmdhryawaql:e3a13e6544bb1423b66553abb91f75caff2ed6ee99b099891d607febdc113792@ec2-35-171-250-21.compute-1.amazonaws.com:5432/d6tm7teu5j5cnc'
 
+db=SQLAlchemy(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -14,7 +18,23 @@ def index():
 
     # Update database if validation success
     if reg_form.validate_on_submit():
-        return "thank you"
+        
+        username=reg_form.username.data 
+        password=reg_form.password.data 
+
+        user_ob=User.query.filter_by(username=username).first()
+        if user_ob :
+            return "user name already exist " 
+
+        user=User(username=username,password=password) 
+        db.session.add(user)
+        db.session.commit()
+
+        return "user created"
+
+
+
+
         
 
     return render_template("register.html", form=reg_form)
